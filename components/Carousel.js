182d7f423fcb407d78cc6f38cb8e59739bda3482
Oleksandr Boitsun <thewip3out@gmail.com>
2021-10-28 09:57:57 +0300

    changes done

--- components/Carousel.js
+++ components/Carousel.js
@@ -12,22 +12,22 @@ import Button from "./Button";
 
 const comments = [
   {
-    author: "John Doe",
+    author: "Morgan Engineering",
     cmnt: "Web Ignite helped me build a website that reflected our business well.",
-    avatar: "/imgs/avatar1.png",
-    href: "/",
+    avatar: "/imgs/pic6.png",
+    bg: "bg-purp",
   },
   {
-    author: "John Doe",
+    author: "Maui Sheep Milk",
     cmnt: "We had a great experience with the website we had done from the team at Web Ignite",
-    avatar: "/imgs/avatar2.png",
-    href: "/",
+    avatar: "/imgs/pic2.png",
+    bg: "bg-ylw",
   },
   {
-    author: "John Doe",
+    author: "City Sanctuary",
     cmnt: "I highly recommend their team for a website that looks great at an affordable price.",
-    avatar: "/imgs/avatar3.png",
-    href: "/",
+    avatar: "/imgs/pic3.png",
+    bg: "bg-grn",
   },
 ];
 export default function Carousel() {
@@ -48,9 +48,9 @@ export default function Carousel() {
         >
           {comments.map((c, i) => (
             <SwiperSlide key={i} className="w-max">
-              <div className="relative pt-20 pb-10">
-                <div className="w-40 h-40 rounded-full overflow-hidden absolute top-0 left-1/2 transform -translate-x-1/2  z-[1]">
-                  <img className="object-cover" src={c.avatar} alt={c.author} />
+              <div className="relative  pb-10">
+                <div className={`carousel-avatar ${c.bg}`}>
+                  <img className="w-full top-0 left-0" src={c.avatar} alt={c.author} />
                 </div>
                 <div className="w-full max-w-[280px] bg-white relative rounded-[20px] min-h-[250px] h-auto mx-auto pt-20 pb-5 px-5 flex flex-col justify-end items-center">
                   <p className="text-center font-semibold text-sm my-auto">“{c.cmnt}”</p>
@@ -76,9 +76,9 @@ export default function Carousel() {
         >
           {comments.map((c, i) => (
             <SwiperSlide key={i} className="w-max ">
-              <div className="relative pt-20 pb-10">
-                <div className="w-40 h-40 rounded-full overflow-hidden absolute top-0 left-1/2 transform -translate-x-1/2  z-[1]">
-                  <img className="object-cover" src={c.avatar} alt={c.author} />
+              <div className="relative  pb-10">
+                <div className={`carousel-avatar ${c.bg}`}>
+                  <img className="w-full top-0 left-0" src={c.avatar} alt={c.author} />
                 </div>
                 <div className="w-full  bg-white relative rounded-[20px] min-h-[250px] h-auto mx-auto pt-20 pb-5 px-5 flex flex-col justify-end items-center">
                   <p className="text-center font-semibold text-sm my-auto">“{c.cmnt}”</p>
