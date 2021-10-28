520e55cc8a4067ce3b855c4a832c43400b29c57f
Oleksandr Boitsun <thewip3out@gmail.com>
2021-10-28 10:14:29 +0300

    changes done

--- components/Form.js
+++ components/Form.js
@@ -18,7 +18,7 @@ export default function Form() {
       email,
       message,
     };
-    fetch("https://webignite.vercel.app/api/hello", {
+    fetch("/api/hello", {
       method: "POST",
       headers: {
         Accept: "application/json, text/plain, */*",
